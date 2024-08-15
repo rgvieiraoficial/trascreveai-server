a35cf22b5bb31d7051be778c2609bc4bb7d2ca2f
rgvieira <yami.hiei87@gmail.com>
2024-08-15 16:58:11 -0300

    Remove footer

--- src/modules/whatsapp/useCases/receiveTextMessage/ReceiveTextMessageUseCase.ts
+++ src/modules/whatsapp/useCases/receiveTextMessage/ReceiveTextMessageUseCase.ts
@@ -87,7 +87,6 @@ class ReceiveTextMessageUseCase {
           interactive: {
             type: message.type,
             body: message.body,
-            footer: message.footer,
             action: message.action
           }
         }
