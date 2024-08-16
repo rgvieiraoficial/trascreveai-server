4f7f4ab82be601f4445da97a2856c4c674d943cb
rgvieira <yami.hiei87@gmail.com>
2024-08-16 00:53:18 -0300

    Implement Audio Message UseCase

--- src/modules/whatsapp/useCases/receiveAudioMessage/ReceiveAudioMessageUseCase.ts
+++ src/modules/whatsapp/useCases/receiveAudioMessage/ReceiveAudioMessageUseCase.ts
@@ -1,11 +1,15 @@
-import path from 'path';
+import { Contact, SessionTasks } from '@prisma/client';
 
-import { AxiosResponse } from 'axios';
+import { IBotMessagesTemplatesRepository } from '../../../bot/repositories/IBotMessagesTemplatesRepository';
+import { IContactsRepository } from '../../../contacts/repositories/IContactsRepository';
+import { ISessionTasksRepository } from '../../../sessions_tasks/repositories/ISessionTasksRepository';
 
-import { graphApi } from '../../../../lib/graphApi';
+import { IMessageContent } from 'modules/whatsapp/types/whastsappTypes';
 
 import { audioTranscribe } from '../../utils/audioTranscribe';
-import downloadAudioFromMeta from '.././../utils/downloadAudioFromMeta';
+import downloadAudioFromMeta from '../../utils/downloadAudioFromMeta';
+import markMessageAsRead from '../../utils/markMessageAsRead';
+import sendMessageToWhatsAppContact from '../../utils/sendMessageToWhatsAppContact';
 
 interface IRequest {
   name: string;
@@ -18,102 +22,70 @@ interface IRequest {
   };
 }
 
-interface IGetMediaURLData {
-  data: {
-    messaging_product: string
-    id: string,
-    file_size: number,
-    mime_type: string,
-    url: string;
-    sha256: string,
-  }
-}
-
-interface ISendMessageData {
-  messaging_product: string;
-  to?: string;
-  status?: string;
-  text?: {
-    body: string;
-  };
-  message_id?: string;
-  messages?: [
-    {
-      id: string;
-    }
-  ]
-}
-
 class ReceiveAudioMessageUseCase {
+
+  constructor(
+    private botsMessagesTemplatesRepository: IBotMessagesTemplatesRepository,
+    private contactsRepository: IContactsRepository,
+    private sessionsTasksRepository: ISessionTasksRepository
+  ) { }
 
   async execute({ name, phone_number_id, from, message_id, audio }: IRequest): Promise<void> {
     console.log(`ID: ${message_id}`);
 
     console.log(`Audio ID: ${audio.id}`);
 
-    const token = process.env.WHATSAPP_TOKEN;
+    await markMessageAsRead(phone_number_id, message_id);
 
-    const url = `/${phone_number_id}/messages?access_token=${token}`;
+    const contactExists = await this.contactsRepository.findByPhoneNumberId(phone_number_id);
 
-    const fist_reply_data: ISendMessageData = {
-      messaging_product: "whatsapp",
-      to: from,
-      text: {
-        body: 'Transcrevendo áudio...'
-      },
-    };
+    let contact: Contact = null;
 
-    try {
-      await graphApi.post<ISendMessageData, AxiosResponse<ISendMessageData>>(url, fist_reply_data);
-    } catch (err) {
-      console.log(err.message);
+    if (!contactExists) {
+      contact = await this.contactsRepository.create({
+        name,
+        whatsapp_number: from,
+        phone_number_id,
+      });
+    } else {
+      contact = contactExists;
     }
 
-    const header_config = {
-      headers: { Authorization: `Bearer ${token}` }
-    };
-
-    const read_data: ISendMessageData = {
-      messaging_product: "whatsapp",
-      status: "read",
-      message_id,
-    };
-
-    try {
-      await graphApi.post<ISendMessageData, AxiosResponse<ISendMessageData>>(url, read_data, header_config);
-    } catch (err) {
-      console.log(err.message);
-    }
-
-    const get_media_url = `/${audio.id}?phone_number_id=${phone_number_id}`;
-
-    try {
-      const media_url: IGetMediaURLData = await graphApi.get(get_media_url, header_config);
-
-      const file_name = `audio_${Date.now().toString()}.ogg`;
-
-      const file_path = path.resolve('tmp', 'media', 'audios', file_name);
-
-      await downloadAudioFromMeta(media_url.data.url, file_path);
-
-      const transcript = await audioTranscribe(file_path);
-
-      const reply_data: ISendMessageData = {
-        messaging_product: "whatsapp",
-        to: from,
-        text: {
-          body: `*${transcript}*`
-        },
-      };
-
-      try {
-        await graphApi.post<ISendMessageData, AxiosResponse<ISendMessageData>>(url, reply_data);
-      } catch (err) {
-        console.log(err.message);
+    await sendMessageToWhatsAppContact(phone_number_id, contact.whatsapp_number, [{
+      type: 'text',
+      body: {
+        text: 'Transcrevendo áudio...'
       }
-    } catch (err) {
-      console.log(err.message);
+    }]);
+
+    let session_task: SessionTasks = null;
+
+    const sessionTaskExists = await this.sessionsTasksRepository.findOpenSessionTasksByContact(contact.id);
+
+    if (!sessionTaskExists || sessionTaskExists.status === 2) {
+      session_task = await this.sessionsTasksRepository.create({
+        status: 1,
+        stage: 'welcome-message',
+        contact: {
+          connect: {
+            id: contact.id
+          }
+        }
+      });
+    } else {
+      session_task = sessionTaskExists;
     }
+
+    const file_path = await downloadAudioFromMeta(phone_number_id, audio.id);
+
+    const transcript = await audioTranscribe(file_path);
+
+    await sendMessageToWhatsAppContact(phone_number_id, contact.whatsapp_number, [{
+      type: 'text',
+      body: {
+        text: `*${transcript}*`
+      }
+    }]);
   }
 }
 
--- src/modules/whatsapp/utils/downloadAudioFromMeta.ts
+++ src/modules/whatsapp/utils/downloadAudioFromMeta.ts
@@ -1,15 +1,41 @@
 import fs from 'fs';
+import path from 'path';
 import * as stream from 'stream';
 import { promisify } from 'util';
 
 import { graphApi } from '../../../lib/graphApi';
 
+interface IGetMediaURLData {
+  data: {
+    messaging_product: string
+    id: string,
+    file_size: number,
+    mime_type: string,
+    url: string;
+    sha256: string,
+  }
+}
+
 const pipeline = promisify(stream.pipeline);
 
-const downloadAudioFromMeta = async (url: string, file_name: string) => {
+const downloadAudioFromMeta = async (phone_number_id: string, audio_id: string): Promise<string> => {
   const token = process.env.WHATSAPP_TOKEN;
 
+  const get_media_url = `/${audio_id}?phone_number_id=${phone_number_id}`;
+
+  const header_config = {
+    headers: { Authorization: `Bearer ${token}` }
+  };
+
   try {
+    const media_url: IGetMediaURLData = await graphApi.get(get_media_url, header_config);
+
+    const file_name = `audio_${Date.now().toString()}.ogg`;
+
+    const file_path = path.resolve('tmp', 'media', 'audios', file_name);
+
+    const url = media_url.data.url;
+
     const response = await graphApi.get(url, {
       responseType: 'stream',
       headers: {
@@ -19,6 +45,8 @@ const downloadAudioFromMeta = async (url: string, file_name: string) => {
     });
     await pipeline(response.data, fs.createWriteStream(file_name));
     console.log('Download Completed');
+
+    return file_path;
   } catch (error) {
     console.error('Download audio pipeline failed', error);
   }
